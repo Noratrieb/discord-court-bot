d6ed409c9be313303d4512c77b9eb3b97f2a747b
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-06-14 18:52:46 +0200

    thing

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # discord-court-bot
 a very funny discord bot
+
+
+.env
+```
+DISCORD_TOKEN=token
+APPLICATION_ID=uwu
+RUST_LOG=DEBUG
+MONGO_URI=mongodb://localhost:27017
+DEV=
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+mod handler;
+
+use std::env;
+
+use color_eyre::{eyre::WrapErr, Result};
+use serenity::{model::prelude::*, prelude::*};
+
+use crate::handler::Handler;
+
+#[tokio::main]
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let _ = dotenv::dotenv();
+
+    let token = env::var("DISCORD_TOKEN").wrap_err("DISCORD_TOKEN not found in environment")?;
+    let guild_id = if let Ok(_) = env::var("DEV") {
+           SOme( GuildId(
+                env::var("GUILD_ID")
+                    .wrap_err("GUILD_ID not found in environment, must be set when DEV is set")?
+                    .parse()
+                    .wrap_err("GUILD_ID must be an integer")?,
+            ))
+        })
+} else {None};
+
+    let mut client = Client::builder(token, GatewayIntents::empty())
+        .event_handler(Handler { dev_guild_id })
+        .await
+        .wrap_err("failed to create discord client")?;
+
+    client.start().await.wrap_err("running client")
 }
